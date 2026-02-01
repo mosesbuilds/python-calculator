8acc453dc1721af7da019de872c54e94d62404b0
Moses Agbahowe <mosesagbahowe@gmail.com>
2026-02-01 17:53:34 +0100

    Add files via upload

---
+++ welding_calculator.py
@@ -0,0 +1,8 @@
+price = float(input("Enter price per unit: "))
+quantity = float(input("Enter quantity needed: "))
+
+total = price * quantity
+
+print("-----------")
+print("Total cost is:", total)
+print("Thank you for using Moses Welding Calculator")

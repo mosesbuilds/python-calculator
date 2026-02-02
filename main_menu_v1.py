3d2d6a6cbbf42118e58083714a98507d712754c1
Moses Agbahowe <mosesagbahowe@gmail.com>
2026-02-02 20:40:35 +0100

    Add files via upload

---
+++ main_menu_v1.py
@@ -0,0 +1,18 @@
+print("==== Moses Welding Tools ====")
+print("1. Basic Welding Calculator")
+print("2. Project Cost Calculator")
+print("3. Daily Welding Log")
+print("4. Exit")
+
+choice = input("Choose an option: ")
+
+if choice == "1":
+    print("Opening Basic Welding Calculator...")
+elif choice == "2":
+    print("Opening Project Cost Calculator...")
+elif choice == "3":
+    print("Opening Daily Welding Log...")
+elif choice == "4":
+    print("Goodbye!")
+else:
+    print("Invalid option selected.")
